9a3e40bb25855913c0949079c52ddefa2f02b2cf
Andrius <andrius.inside@gmail.com>
2018-11-01 20:40:25 +0200

    generation of grid 9x9 is working with bugs

--- table.php
+++ table.php
@@ -25,22 +25,86 @@ class table {
         0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0
     );
+    private $possible = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    public function getPossible() {
+        return $this->possible;
+    }
+
+    public function removeFromPossible($arr, $number_to_remove) {
+        $modified = $arr;
+        if (!in_array($number_to_remove, $modified)) {
+            return $modified;
+        }
+        unset($modified[array_keys($modified, $number_to_remove)[0]]);
+        sort($modified);
+        return $modified;
+    }
+
+    public function getRandomNumber($arr) {
+        if (!empty($arr)) {
+            return $arr[rand(0, sizeof($arr) - 1)];
+        }
+        return 0;
+    }
+
+    public function getPossibleValues($cell, $table) {
+        $possible_cell_values = $this->getPossible();
+        if (empty($table) || empty($possible_cell_values)) {
+            return $possible_cell_values;
+        } else {
+            if ($cell != 0) {
+
+                for ($i = $this->row($cell); $i > 0; $i--) {
+                    $value_to_unset = $table[($this->row($cell) - $i) * 9 + $this->col($cell)];
+                    //echo 'Cell: ' . $cell . '<br/>';
+                    //echo 'Value to remove: ' . $value_to_unset . '<br/>';
+                    if (in_array($value_to_unset, $possible_cell_values)) {
+                        unset($possible_cell_values[array_keys($possible_cell_values, $value_to_unset)[0]]);
+                    }
+                    sort($possible_cell_values);
+                }
+                for ($i = $this->col($cell); $i >= 0; $i--) {
+                    $value_to_unset = $table[$this->row($cell) * 9 + $this->col($cell) - $i];
+                    //echo 'Cell: ' . $cell . '<br/>';
+                    //echo 'Value to remove: ' . $value_to_unset . '<br/>';
+                    if (in_array($value_to_unset, $possible_cell_values)) {
+                        unset($possible_cell_values[array_keys($possible_cell_values, $value_to_unset)[0]]);
+                    }
+                    sort($possible_cell_values);
+                }
+
+                for ($i = 0; $i < 9; $i ++) {
+                    $value_to_unset = $table[floor($this->block($cell) / 3) * 27 + $i % 3 + 9 * floor($i / 3) + 3 * ($this->block($cell) % 3)];
+                    if (in_array($value_to_unset, $possible_cell_values)) {
+                        unset($possible_cell_values[array_keys($possible_cell_values, $value_to_unset)[0]]);
+                    }
+                    sort($possible_cell_values);
+                }
+            }
+            //echo 'Possible values: ';
+            //var_dump($possible_cell_values);
+            //echo '<br/>';
+            //echo 'Number of choises: ' . sizeof($possible_cell_values) . '<br/>';
+            return $possible_cell_values;
+        }
+    }
 
     public function row($cell) {
-        return $cell / 9;
+        return (int) floor($cell / 9);
     }
 
     public function col($cell) {
-        return $cell % 9;
+        return (int) floor($cell % 9);
     }
 
     public function block($cell) {
-        return floor(row($cell) / 3) * 3 + floor(col($cell) / 3);
+        return floor($this->row($cell) / 3) * 3 + floor($this->col($cell) / 3);
     }
 
     public function is_ok_in_row($number, $cell, $table) {
         $ok = true;
-        $row = row($cell);
+        $row = $this->row($cell);
         for ($i = 0; $i < 9; $i++) {
             if ($table[$row * 9 + $i] === $number) {
                 $ok = false;
@@ -52,7 +116,7 @@ class table {
 
     public function is_ok_in_col($number, $cell, $table) {
         $ok = true;
-        $col = col($cell);
+        $col = $this->col($cell);
         for ($i = 0; $i < 9; $i++) {
             if ($table[$col * 9 + $i] === $number) {
                 $ok = false;
@@ -64,84 +128,23 @@ class table {
 
     public function is_ok_in_block($number, $cell, $table) {
         $ok = true;
-        $block = block($cell);
+        $block = $this->block($cell);
+
+        for ($i = 0; $i < 9; $i++) {
+            if ($table[floor($block / 3) * 27 + $i % 3 + 9 * floor($i / 3) + 3 * ($block % 3)] == $number) {
+                $ok = false;
+                break;
+            }
+        }
+        return $ok;
     }
 
-    public static function generate() {
+    public function is_unique($number, $cell, $table) {
+        return $this->is_ok_in_row($number, $cell, $table) and $this->is_ok_in_col($number, $cell, $table) and $this->is_ok_in_block($number, $cell, $table);
+    }
 
-        // Number of cells in one line
-        $cell_count = 9;
-
-        // A table size is cell_count*cell_count
-        $table = array();
-
-        for ($i = 0; $i < $cell_count; $i++) {
-
-            $row = array();
-            for ($j = 0; $j < $cell_count; $j++) {
-
-                $num_selected = false;
-                $num = rand(1, 9);
-                while (!$num_selected) {
-
-                    if (empty($table) && empty($row)) {
-                        $num_selected = true;
-                    }
-
-                    if (empty($table) && !empty($row)) {
-                        if (!in_array($num, $row)) {
-                            $num_selected = true;
-                        } else {
-                            $num = rand(1, 9);
-                            continue;
-                        }
-                    }
-
-                    if (!empty($table)) {
-
-                        for ($k = 0; $k < sizeof($table); $k++) {
-
-                            if ($table[$k][$j] == $num || in_array($num, $row)) {
-                                $num_selected = false;
-                                $num = rand(1, 9);
-                                break;
-                            } else {
-                                $num_selected = true;
-                            }
-                        }
-                    }
-                }
-
-                array_push($row, $num);
-            }
-
-            array_push($table, $row);
-        }
-
-        $col1 = array();
-        $col2 = array();
-        $col3 = array();
-        echo '<pre>';
-        var_dump(sizeof($table));
-        for ($k = 0; $k < sizeof($table); $k++) {
-            for ($l = 0; $l < sizeof($table); $l++) {
-                if ($l == 0) {
-                    array_push($col1, $table[$k][$l]);
-                }
-
-                if ($l == 1) {
-                    array_push($col2, $table[$k][$l]);
-                }
-
-                if ($l == 2) {
-                    array_push($col3, $table[$k][$l]);
-                }
-            }
-        }
-//        echo 'Column 2: ';
-//        var_dump($col2);
-//        echo '</pre>';
-        return $table;
+    public function getTable() {
+        return $this->table;
     }
 
 }
