c0a65b53a06e408f4eb7286053fe5327548237fa
Andrius <andrius.inside@gmail.com>
2019-09-05 20:42:23 +0300

    Saving to a cookie added: defect fixed when adding to cookie_value array

--- services/save.php
+++ services/save.php
@@ -6,8 +6,8 @@
 // Headers
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-    // you want to allow, and if so:
+// Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+// you want to allow, and if so:
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
@@ -17,7 +17,7 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        // may also be using PUT, PATCH, HEAD etc
+// may also be using PUT, PATCH, HEAD etc
         header("Access-Control-Allow-Methods: POST");
     }
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
@@ -46,27 +46,28 @@ $cookie_name = 'com_game_sudoku';
 // As player can play multiple puzzles
 // Restrict available amount of saves, for example no more than 5 saves
 // Let user to choose, which play_id to fetch from cookie
-
 // Do not use serialize/unserialize (https://stackoverflow.com/questions/9032007/arrays-in-cookies-php)
 if (isset($_COOKIE[$cookie_name])) {
     $cookie_value = json_decode($_COOKIE[$cookie_name], true);
-    $key = key($cookie_value);
-    if ($cookie_value[$key]['initial'] === $initial) {
-        $cookie_value = array(
-            $key => array('initial' => $cookie_value[$key]['initial'],
-                'player_inputs' => $player_inputs)
-        );
-        setcookie($cookie_name, json_encode($cookie_value), time() + 180, "/"); // 3min
-    } else {
-        $new_key = time() . rand(1, 9);
-        array_push($cookie_value, array(
-            $new_key => array(
-                'initial' => $initial,
-                'player_inputs' => $player_inputs
-            )
-        ));
-        setcookie($cookie_name, json_encode($cookie_value), time() + 180, "/"); // 3min
+    $keys = array_keys($cookie_value);
+    foreach ($keys as $key) {
+        if ($cookie_value[$key]['initial'] === $initial) {
+            $cookie_value = array(
+                $key => array('initial' => $cookie_value[$key]['initial'],
+                    'player_inputs' => $player_inputs)
+            );
+            setcookie($cookie_name, json_encode($cookie_value), time() + 180, "/"); // 3min
+        } else {
+            continue;
+        }
     }
+
+    $new_key = time() . rand(1, 9);
+    $cookie_value[$new_key] = array(
+        'initial' => $initial,
+        'player_inputs' => $player_inputs
+    );
+    setcookie($cookie_name, json_encode($cookie_value), time() + 180, "/"); // 3min
 } else {
     $cookie_value = array(
         time() . rand(1, 9) => array('initial' => $initial,
